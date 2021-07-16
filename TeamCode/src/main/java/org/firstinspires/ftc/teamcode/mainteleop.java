08ed24db55a77462f7955fa6df80b67caa384ec1
Ely31 <78882466+Ely31@users.noreply.github.com>
2021-07-16 13:09:53 -0500

    cleaned up the code
    luke's code was messy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainteleop.java
@@ -7,35 +7,36 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class mainteleop extends LinearOpMode {
-
-    DcMotor FR = null;
-    DcMotor FL = null;
-    DcMotor BR = null;
-    DcMotor BL = null;
-    DcMotor shooter = null;
-    DcMotor wobbleArm = null;
-    DcMotor collectionMechanism = null;
-    DcMotor neat = null;
-    Servo ringPusher = null;
-    Servo wobbleGoalGrabber = null;
+    // declare motors and servos
+    DcMotor FL, FR, BL, BR;
+    DcMotor shooter;
+    DcMotor wobbleArm;
+    DcMotor collectionMechanism;
+    DcMotor neat;
+    Servo ringPusher;
+    Servo wobbleGoalGrabber;
 
     public void runOpMode() {
-
+        // initialization
+        // hardwaremap
+        // motors
         FR = hardwareMap.get(DcMotor.class, "FR");
         FL = hardwareMap.get(DcMotor.class, "FL");
         BR = hardwareMap.get(DcMotor.class, "BR");
         BL = hardwareMap.get(DcMotor.class, "BL");
+        FL.setDirection(DcMotor.Direction.REVERSE);
+        BL.setDirection(DcMotor.Direction.REVERSE);
+
         shooter = hardwareMap.get(DcMotor.class, "shooter");
         collectionMechanism = hardwareMap.get(DcMotor.class, "collectionMechanism");
         wobbleArm = hardwareMap.get(DcMotor.class, "wobbleArm");
         neat = hardwareMap.get(DcMotor.class, "neat");
-
+        // servos
         wobbleGoalGrabber = hardwareMap.get(Servo.class, "wobbleGoalGrabber");
         ringPusher = hardwareMap.get(Servo.class, "ringPusher");
         ringPusher.setPosition(0.95);
 
-        FL.setDirection(DcMotor.Direction.REVERSE);
-        BL.setDirection(DcMotor.Direction.REVERSE);
+        double drivepow;
         double intakepow;
         boolean prevy = false;
         boolean prevx = false;
@@ -43,38 +44,42 @@ public class mainteleop extends LinearOpMode {
         boolean intaketoggle = false;
         boolean shootertoggle = false;
         boolean clawtoggle = false;
+
+        // teleop loop starts
         waitForStart();
         while (opModeIsActive()) {
+            // slow drive button
+            if (gamepad1.right_trigger < 0.1) drivepow = 1;
+            else drivepow = 0.125;
 
+            // mecanum stuff
             double r = Math.hypot(gamepad1.left_stick_x, -1 * gamepad1.left_stick_y);
             double robotAngle = Math.atan2(-1 * gamepad1.left_stick_y, gamepad1.left_stick_x) + -20;
             double rightX = gamepad1.right_stick_x;
-            final double v1 = r * Math.cos(robotAngle) + rightX;
-            final double v2 = r * Math.sin(robotAngle) - rightX;
-            final double v3 = r * Math.sin(robotAngle) + rightX;
-            final double v4 = r * Math.cos(robotAngle) - rightX;
+            final double v1 = (r * Math.cos(robotAngle) + rightX) * drivepow;
+            final double v2 = (r * Math.sin(robotAngle) - rightX) * drivepow;
+            final double v3 = (r * Math.sin(robotAngle) + rightX) * drivepow;
+            final double v4 = (r * Math.cos(robotAngle) - rightX) * drivepow;
+            FL.setPower(v1);
+            FR.setPower(v2);
+            BL.setPower(v3);
+            BR.setPower(v4);
 
             // shooter toggler
-            if (gamepad2.b && !prevb) { // if b pressed and prevx is false
-                shootertoggle = !shootertoggle; // toggle shootertoggle when b is pressed
-            }
+            // if b pressed and prevx is false
+            if (gamepad2.b && !prevb) shootertoggle = !shootertoggle; // toggle shootertoggle when b is pressed
             prevb = gamepad2.b;
 
             if (shootertoggle) { // run shooter if shootertoggle is true
                 shooter.setPower(-0.5);
-            } else {
-                shooter.setPower(0);
-            }
+            } else shooter.setPower(0);
 
             // reverse intake if a is pressed
-            if (gamepad2.a){
-                intakepow = -1;
-            } else intakepow = 1;
+            if (gamepad2.a) intakepow = -1;
+            else intakepow = 1;
 
             // intake toggler
-            if (gamepad2.x && !prevx) { // if x pressed and prevx is false
-                intaketoggle = !intaketoggle;
-            }
+            if (gamepad2.x && !prevx) intaketoggle = !intaketoggle; // if x pressed and prevx is false
             prevx = gamepad2.x;
             if (intaketoggle) {
                 collectionMechanism.setPower(intakepow);
@@ -84,44 +89,21 @@ public class mainteleop extends LinearOpMode {
                 neat.setPower(0);
             }
             // shoot a ring when a is pressed
-            if (gamepad1.left_trigger > 0.1) {
-                ringPusher.setPosition(0.6);
-            }
-                else {ringPusher.setPosition(0.95);
-            }
+            if (gamepad1.left_trigger > 0.1) ringPusher.setPosition(0.6);
+                else ringPusher.setPosition(0.95);
 
             // toggle clawtoggle if y pressed
-            if (gamepad2.y && !prevy) {
-                clawtoggle = !clawtoggle;
-            }
+            if (gamepad2.y && !prevy) clawtoggle = !clawtoggle;
             prevy = gamepad2.y;
             // open wobble claw when toggle is true
-            if (clawtoggle) {
-                wobbleGoalGrabber.setPosition(1);
-            } else {
-                wobbleGoalGrabber.setPosition(0.25);
-            }
+            if (clawtoggle) wobbleGoalGrabber.setPosition(1);
+            else wobbleGoalGrabber.setPosition(0.25);
 
             // control wobble arm with dpad
-            if (gamepad2.dpad_down) {
-                wobbleArm.setPower(-0.25);
-            } else if (gamepad2.dpad_up){
-                wobbleArm.setPower(0.25);
-            } else wobbleArm.setPower(0);
+            if (gamepad2.dpad_down) wobbleArm.setPower(-0.25);
+            else if (gamepad2.dpad_up) wobbleArm.setPower(0.25);
+            else wobbleArm.setPower(0);
 
-            // slow mode
-            if (gamepad1.right_trigger < 0.1) { // if left trigger not pressed
-                FL.setPower(v1);
-                FR.setPower(v2);
-                BL.setPower(v3);
-                BR.setPower(v4);
-            } else if (gamepad1.right_trigger > 0.1) { // if left trigger pressed
-                FL.setPower(v1 / 8);
-                FR.setPower(v2 / 8);
-                BL.setPower(v3 / 8);
-                BR.setPower(v4 / 8);
-                // drive slow
-            }
             telemetry.update();
         }
     }
